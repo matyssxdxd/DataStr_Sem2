655730599726b24ed98635584049dcf08c67779f
Matīss Purviņš <s22purvmati@vea.lv>
2024-03-13 11:38:16 +0200

    MyListNode complete??

--- src/datastr/MyListNode.java
+++ src/datastr/MyListNode.java
@@ -1,4 +1,45 @@
 package datastr;
 
-public class MyListNode {
+public class MyListNode<T> {
+    // TODO:
+    //  1. variables - element, next un previous
+    //  2. get and set
+    //  3. constructors
+    //  4. toString
+
+    private T element;
+    private MyListNode<T> next = null;
+    private MyListNode<T> previous = null;
+
+    public T getElement() {
+        return element;
+    }
+
+    public MyListNode<T> getNext() {
+        return next;
+    }
+
+    public MyListNode<T> getPrevious() {
+        return previous;
+    }
+
+    public void setElement(T element) {
+        this.element = element != null ? element : (T) new Object();
+    }
+
+    public void setNext(MyListNode<T> next) {
+        this.next = next;
+    }
+
+    public void setPrevious(MyListNode<T> previous) {
+        this.previous = previous;
+    }
+
+    public MyListNode(T element) {
+        setElement(element);
+    }
+
+    public String toString() {
+        return "" + element;
+    }
 }
