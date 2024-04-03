43620825de4a3a3508876a9a25eb90bb89c51878
localuser <localuser@VEA.LV>
2024-04-03 10:28:54 +0300

    find and delete

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -1,5 +1,8 @@
 package datastr;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MyLinkedList<Ttype> {
 	private MyListNode first = null;
 	private MyListNode last = null;
@@ -76,7 +79,54 @@ public class MyLinkedList<Ttype> {
 
 		}
 	}
-	
+
+	public void delete(int pos) throws Exception {
+		if (isEmpty()) throw new Exception("There are no elements in list");
+		if (pos >= counter || pos < 0) throw new Exception("Problems with pos");
+
+		if (pos == 0) {
+			first = first.getNext();
+			first.setPrevious(null);
+			counter--;
+			System.gc();
+		} else if (pos == counter - 1) {
+			last = last.getPrevious();
+			last.setNext(null);
+			counter--;
+			System.gc();
+		} else {
+			MyListNode temp = first;
+
+			for (int i = 0; i < pos; i++) {
+				temp = temp.getNext();
+			}
+
+			MyListNode rightNode = temp;
+			MyListNode leftNode = temp.getPrevious();
+
+			leftNode.setNext(rightNode.getNext());
+			counter--;
+			System.gc();
+		}
+	}
+
+	public ArrayList<Integer> find(Ttype element) throws Exception {
+		if (element == null) throw new Exception("Problems with element");
+
+		MyListNode temp = first;
+		ArrayList<Integer> result = new ArrayList<>();
+
+		for (int i = 0; i < counter; i++) {
+			if (temp.getElement() == element) {
+				result.add(i);
+			}
+			temp = temp.getNext();
+		}
+
+		if (result.isEmpty()) throw new Exception("There is no " + element + " in list");
+
+		return result;
+	}
 	
 	public void print() throws Exception
 	{
--- src/service/MainService.java
+++ src/service/MainService.java
@@ -9,9 +9,14 @@ public class MainService {
 		try {
 			myList.add(23);
 			myList.add(100);
+			myList.add(100);
+			myList.add(100);
 			myList.add(-5);
 			myList.add(10, 2);
 			myList.print();
+			myList.delete(3);
+			myList.print();
+			System.out.println(myList.find(100));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
